237eb00c735e338c10d3e6c1ad9fe9d97101fc56
Casey Dailey <casey@feasttogether.org>
2017-02-03 11:55:14 -0600

    working, but not technically correct

--- javascripts/iife1.js
+++ javascripts/iife1.js
@@ -10,17 +10,18 @@ var CarLot = (function(otherCarLot){
 		var inventory = [];
 
 		var dataRequest = new XMLHttpRequest();
+		dataRequest.open("GET", "../inventory.json");
+		dataRequest.send();
 
+		/////// open and send the request
 		dataRequest.addEventListener('load', loadComplete);
 		dataRequest.addEventListener('error', loadFailed);
 
-		// This parses the JSON and passes it to storeCars
 		function loadComplete(load){
 			let carData = JSON.parse(load.target.responseText);
 			storeCars(carData, inventory);
 		}
 
-		// logs the fail message
 		function loadFailed(load) {
 			console.log("data failed", load.target.responseText);
 		}
@@ -41,29 +42,19 @@ var CarLot = (function(otherCarLot){
 
 		
 		function  displayCars(array) {
-			
-    		// var rowOne = document.createElement("section");
-    		// rowOne.classList.add("row");
-    		// gallery.appendChild(rowOne);
-    	
-    		// var rowTwo = document.createElement("section");
-    		// rowTwo.classList.add("row");
-    		// gallery.appendChild(rowTwo);
 
 			var counter = 0;
 			array.forEach(car => {
 
-				card += `<div class="col-lg-4 col-sm-4 col-xs-6 black-border">
+				card += `<div class="col-lg-4 col-sm-4 col-xs-6 card">
     					 <h4>${car.year} ${car.make} ${car.model}</h4>
     					 <img class="thumbnail img-responsive" src="../${car.image}">
-    					 <p class="${counter}">${car.description}</p>
+    					 <p class="description">${car.description}</p>
     					 <p><small>${car.price}</small></p>
     					 </div>`;
 
     				counter++;
-    				console.log('counter:', counter);
-    				console.log('card:', card);
-
+    				
     				if (counter % 3 === 0) {
     					var rowCount = 1;
     					gallery.innerHTML += `<section class="row">${card}</section>`;
@@ -79,9 +70,9 @@ var CarLot = (function(otherCarLot){
 		return inventory;
 	};
 
-	/////// open and send the request
-	dataRequest.open("GET", "../inventory.json");
-	dataRequest.send();
+	// /////// open and send the request
+	// dataRequest.open("GET", "../inventory.json");
+	// dataRequest.send();
 
 	/////// returns augmented iife
 	return otherCarLot;
--- javascripts/iife2.js
+++ javascripts/iife2.js
@@ -1,2 +1,47 @@
 'use strict';
+console.log('iife 2 is linked');
 
+var input = document.getElementById("input");
+// var body = document.querySelector("body");
+
+var CarLot = (function(otherCarLot){
+
+		gallery.addEventListener("click", activateCard);
+		input.addEventListener("keyup", reWriteCard);
+		input.addEventListener("keypress", deActivateCard);
+		
+
+		function activateCard (e) {
+	
+			if (e.target.parentNode.classList.contains("card")) {
+				let cardsArray = Array.from(document.getElementsByClassName("card"));
+				cardsArray.forEach((card) => card.classList.remove("active"));
+				e.target.parentNode.classList.add("active");
+				input.focus();
+				input.value = "";
+			}
+		}
+
+		function reWriteCard (e) {
+			let inputValue = input.value;
+			let card = document.querySelector(".active");
+			let description = card.querySelector(".description");
+			description.innerHTML = inputValue;
+		}
+
+		function deActivateCard(e){
+			if (e.keyCode === 13) {
+				let cardsArray = Array.from(document.getElementsByClassName("card"));
+				cardsArray.forEach((card) => card.classList.remove("active"));
+				input.value = "";
+				input.blur();
+			}
+		}
+
+
+
+
+return otherCarLot;
+
+
+})(CarLot || {});
