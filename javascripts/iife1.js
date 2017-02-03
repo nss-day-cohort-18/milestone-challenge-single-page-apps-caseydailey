7a758454a4c86ac628aef3db067c6e9d28cc81e7
Casey Dailey <casey@feasttogether.org>
2017-02-03 08:30:05 -0600

    ok. now it time to break it up

--- javascripts/iife1.js
+++ javascripts/iife1.js
@@ -2,8 +2,6 @@
 console.log('CarLot iife #1 is linked');
 
 var gallery = document.getElementById("gallery");
-// var rowOne = document.getElementById("row-one");
-// var rowTwo = document.getElementById("row-two");
 var carsArray = "";
 var card = "";
 
@@ -44,20 +42,18 @@ var CarLot = (function(otherCarLot){
 		
 		function  displayCars(array) {
 			
-    		var rowOne = document.createElement("section");
-    		console.log('rowOne:', rowOne);
-    		rowOne.classList.add("row");
-    		console.log('rowOne:', rowOne);
-    		gallery.appendChild(rowOne);
+    		// var rowOne = document.createElement("section");
+    		// rowOne.classList.add("row");
+    		// gallery.appendChild(rowOne);
     	
-    		var rowTwo = document.createElement("section");
-    		rowTwo.classList.add("row");
-    		gallery.appendChild(rowTwo);
+    		// var rowTwo = document.createElement("section");
+    		// rowTwo.classList.add("row");
+    		// gallery.appendChild(rowTwo);
 
 			var counter = 0;
 			array.forEach(car => {
 
-				card = `<div class="col-lg-4 col-sm-4 col-xs-6">
+				card += `<div class="col-lg-4 col-sm-4 col-xs-6 black-border">
     					 <h4>${car.year} ${car.make} ${car.model}</h4>
     					 <img class="thumbnail img-responsive" src="../${car.image}">
     					 <p class="${counter}">${car.description}</p>
@@ -68,13 +64,14 @@ var CarLot = (function(otherCarLot){
     				console.log('counter:', counter);
     				console.log('card:', card);
 
-    				if (counter <= 3) {
-    					rowOne.innerHTML += card; 
-    				}else if(counter > 3){ 
-    					rowTwo.innerHTML += card;
+    				if (counter % 3 === 0) {
+    					var rowCount = 1;
+    					gallery.innerHTML += `<section class="row">${card}</section>`;
+    					rowCount ++;
+    					card = ""; 
     				}
 			}); 
-					
+
 		}//end display function
 
 	// public method to get inventory 
