99081015dbfa2615528a85146cf4382c336a3d25
Casey Dailey <casey@feasttogether.org>
2017-02-03 15:14:26 -0600

    MVP meeting all technical requirements

--- javascripts/iife2.js
+++ javascripts/iife2.js
@@ -1,42 +1,26 @@
 'use strict';
-console.log('iife 2 is linked');
-
-var input = document.getElementById("input");
-// var body = document.querySelector("body");
 
 var CarLot = (function(otherCarLot){
 
-		gallery.addEventListener("click", activateCard);
-		input.addEventListener("keyup", reWriteCard);
-		input.addEventListener("keypress", deActivateCard);
-		
+	otherCarLot.activateEvents = function(){
 
-		function activateCard (e) {
-	
-			if (e.target.parentNode.classList.contains("card")) {
-				let cardsArray = Array.from(document.getElementsByClassName("card"));
-				cardsArray.forEach((card) => card.classList.remove("active"));
-				e.target.parentNode.classList.add("active");
-				input.focus();
-				input.value = "";
-			}
-		}
+		dataRequest.addEventListener('load', (e)=>{
+			CarLot.loadComplete(e);
+		});
+		dataRequest.addEventListener('error', (e)=>{
+			CarLot.loadFailed(e);
+		});
 
-		function reWriteCard (e) {
-			let inputValue = input.value;
-			let card = document.querySelector(".active");
-			let description = card.querySelector(".description");
-			description.innerHTML = inputValue;
-		}
-
-		function deActivateCard(e){
-			if (e.keyCode === 13) {
-				let cardsArray = Array.from(document.getElementsByClassName("card"));
-				cardsArray.forEach((card) => card.classList.remove("active"));
-				input.value = "";
-				input.blur();
-			}
-		}
+		gallery.addEventListener("click", (e)=>{
+			CarLot.activateCard(e);
+		});
+		input.addEventListener("keyup", (e)=>{
+			CarLot.reWriteCard(e);
+		});
+		input.addEventListener("keypress", (e)=>{
+			CarLot.deActivateCard(e);
+		});
+	};
 
 
 
